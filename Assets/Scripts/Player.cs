269c90cd85181d205d1c5ff2b11877e5af670817
Adam Zabenskie <zubs047@gmail.com>
2024-07-17 15:17:13 -0700

    layer collision

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,28 +5,43 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private bool jumpKeyPressed = false;
+    private float horizInput;
+    private Rigidbody rbComponent;
+
+    [SerializeField] private Transform groundCheckTransform = null;
+    [SerializeField] private LayerMask playerMask;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        rbComponent = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             jumpKeyPressed = true;
         }
+
+        horizInput = Input.GetAxis("Horizontal");
     }
 
     // Physics update
     private void FixedUpdate()
     {
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0)
+        {
+            return;
+        }
+
         if (jumpKeyPressed)
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            rbComponent.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
             jumpKeyPressed = false;
         }
+
+        rbComponent.velocity = new Vector3(horizInput, rbComponent.velocity.y, 0);
     }
 }
