292d7f20023649fa8c620943db6b7c5feb2c3989
Adam Zabenskie <zubs047@gmail.com>
2024-07-17 14:41:57 -0700

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private bool jumpKeyPressed = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,17 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            jumpKeyPressed = true;
+        }
+    }
+
+    // Physics update
+    private void FixedUpdate()
+    {
+        if (jumpKeyPressed)
+        {
             GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            jumpKeyPressed = false;
         }
     }
 }
